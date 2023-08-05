2ad3fcfad855786423680987344a33ec58ead60e
Sum000 <115657082+Sum000@users.noreply.github.com>
2023-08-05 21:37:35 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (29.43%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double myPow(double x, int n) {
+         if (n == 0)
+            return 1;
+        
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        
+        return powHelper(x, n);
+    }
+    private double powHelper(double x, int n) {
+        if (n == 0)
+            return 1;
+        
+        double temp = powHelper(x, n / 2);
+        if (n % 2 == 0)
+            return temp * temp;
+        else
+            return x * temp * temp;
+    }
+}
