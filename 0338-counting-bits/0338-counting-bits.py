8de3e4f707b3c74f312c0ff181945cc91c99fbb9
Sum000 <115657082+Sum000@users.noreply.github.com>
2023-09-01 10:03:49 +0530

    Time: 78 ms (69.44%), Space: 23 MB (65.29%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,18 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        li = []
-        if n == 0:
-            return [0]
+        li = [0] * (n + 1)
         
-        li.append(0)
-        
-        for i in range(1, n + 1, 1):
-            no_ones = bin(i).count("1")
-            li.append(no_ones)
+        for i in range(1, n + 1):
+            li[i] = li[i >> 1] + (i & 1)
             
         return li
-    
-    
-    
-    
         
