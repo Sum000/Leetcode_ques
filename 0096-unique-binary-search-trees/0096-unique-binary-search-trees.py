e275534e014644be1b7e35033484656245c8e2ab
Sum000 <115657082+Sum000@users.noreply.github.com>
2023-10-02 23:43:30 +0530

    Time: 110 ms (5.37%), Space: 32.7 MB (21.82%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,7 +1,8 @@
+import numpy
 class Solution:
     def numTrees(self, n: int) -> int:
-        a =  math.factorial(2*n)
-        b = math.factorial(n)
+        a =  numpy.math.factorial(2*n)
+        b = numpy.math.factorial(n)
         x = b * b * (n + 1)
         return a // x
         
