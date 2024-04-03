5dafa30e82850ceee28a307d2ab71e153b6dbe04
Sum000 <115657082+Sum000@users.noreply.github.com>
2024-04-03 12:04:10 +0530

    Time: 534 ms (21.95%), Space: 158.1 MB (26.45%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        cache = {}
+        
+        m, n = len(s), len(t)
+        
+        def dfs(i, j):
+            if j == n:
+                return 1
+            
+            if i == m:
+                return 0
+            
+            if (i, j) in cache:
+                return cache[(i, j)]
+            
+            if s[i] == t[j]:
+                cache[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+            else:
+                cache[(i, j)] = dfs(i + 1, j)
+                
+            return cache[(i, j)]
+        
+        return dfs(0, 0)
+        
+        
+        
